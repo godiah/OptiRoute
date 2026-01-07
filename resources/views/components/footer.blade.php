f8947ced166bf569b5e42db644522cc4b820ae22
Shady <shadrackomuya5@gmail.com>
2026-01-07 21:32:08 +0300

    website details

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -69,8 +69,8 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                         </svg>
-                        <a href="tel:+1234567890" class="hover:text-primary-blue transition duration-300">+1 (234)
-                            567-890</a>
+                        <a href="tel:+1254768743317" class="hover:text-primary-blue transition duration-300">(254)
+                            768-743-317</a>
                     </p>
                     <p class="flex items-center justify-center md:justify-start text-sm text-dark-text opacity-70">
                         <svg class="w-5 h-5 mr-3 text-primary-blue" fill="none" stroke="currentColor"
@@ -80,7 +80,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
-                        <span>123 Tech Street, San Francisco, CA</span>
+                        <span>Nairobi, Kenya</span>
                     </p>
                 </div>
             </div>
